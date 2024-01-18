479d1ef64540f321f9a71f534f1afcec479598bd
FatihaScripter <155731772+fatiha098@users.noreply.github.com>
2024-01-18 20:26:09 +0100

    first commit

--- principal.py
+++ principal.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from subprocess import call
-from tkinter.constants import *
 
 app = Tk()
 app.geometry("800x500")
@@ -14,17 +13,13 @@ background_image = PhotoImage(file = r"c:\Users\hp\Desktop\secondbg.png")
 background_label = Label(app, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-def sales_data_entry():
-  pass
-
-
-
 def purchases():
-
   app.destroy()
-  call(["python", "purchases.py"])
+  call(["python", r"C:\Users\hp\Desktop\tkinter\projet\purchases.py"])
 
-
+def sales():
+  app.destroy()
+  call(["python", r"C:\Users\hp\Desktop\tkinter\projet\sales.py"])
 
 head = Label(app, text="Here You Go To Manage Your Stock",
               font=("Arial", 20, "bold"),
@@ -32,29 +27,22 @@ head = Label(app, text="Here You Go To Manage Your Stock",
               bg="orange",
               pady=20,
               width=300)
-
-label1 =  Label(app, text="Please Choose one of the options :",
-              font=("Arial", 16, "bold"),
-              width="40",
-              bg="black",
-              fg="white")
+head.place(relx=0.5, rely=0.08, anchor=CENTER)
 
 sales_btn = Button(app , text="Sales",width=25,
                   font=("Arial", 17, "bold"), 
                   bg="orange",
                   pady=13
                   )
+sales_btn.place(relx=0.5,rely=0.5, anchor=E)
+sales_btn.config(cursor="hand2")
 
 purchases_btn = Button(app,text="Purchases",width=25, 
                       font=("Arial", 17, "bold"),
                       pady=13, 
                       bg="orange",
                       command=purchases)
-
-head.place(relx=0.5, rely=0.08, anchor=CENTER)
-label1.place(relx=0.5,rely=0.3,anchor=CENTER)
-sales_btn.place(relx=0.5,rely=0.5, anchor=E)
 purchases_btn.place(relx=0.5,rely=0.5, anchor=W)
-
+purchases_btn.config(cursor="hand2")
 
 app.mainloop()
